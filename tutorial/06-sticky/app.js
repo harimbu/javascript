7889bdcf463844c089f98a89e411880f6c9111b3
harimbu <harimbu@gmail.com>
2024-05-27 11:43:15 +0900

    init

--- tutorial/06-sticky/app.js
+++ tutorial/06-sticky/app.js
@@ -2,4 +2,5 @@ const header = document.querySelector('.header')
 
 window.addEventListener('scroll', function(){
     console.log(this.window.scrollY);
+    header.classList.toggle('active', this.window.scrollY > 50)
 })

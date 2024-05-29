656445bb4e16f01caea4fb80fe3075d5d0e2d204
harimbu <harimbu@gmail.com>
2024-05-29 11:19:46 +0900

    init

--- tutorial/12_gallery_app/app.js
+++ tutorial/12_gallery_app/app.js
@@ -6,3 +6,31 @@ const API_KEY = '563492ad6f91700001000001c5e6480855e74c238b20eac4d3d528f9'
 const galleryDiv = document.querySelector('.gallery')
 const form = document.querySelector('form')
 const loadMore = document.querySelector('.load_more')
+
+let pageIndex = 1
+
+function paintImg(photos) {
+    console.log(photos);
+}
+
+async function fetchImg(baseURL) {
+    const res = await fetch(baseURL, {
+        headers : {
+            Authorization : API_KEY
+        }
+    })    
+    const data = await res.json()  
+    return data  
+}
+
+async function getImg(pageIndex) {
+    const baseURL = `https://api.pexels.com/v1/curated?page=${pageIndex}&per_page=12`
+    const data = await fetchImg(baseURL)      
+    paintImg(data.photos)  
+}
+
+function init() {
+    getImg(pageIndex)
+}
+
+init()

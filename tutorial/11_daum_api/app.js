353c13990e1ee96acb09ba55654a45f372a141d1
harimbu <harimbu@gmail.com>
2024-05-29 10:24:48 +0900

    init

--- tutorial/11_daum_api/app.js
+++ tutorial/11_daum_api/app.js
@@ -7,20 +7,44 @@ const input = document.querySelector('form input')
 const btn = document.getElementById('btn')
 const bookContainer = document.querySelector('.book_container')
 
+let bookName = 'html'
+
 function getBooks() {
     $.ajax(
         {
             method: 'GET',
             url : 'https://dapi.kakao.com/v3/search/book',
             data: {
-                query: '안녕', 
+                query: bookName, 
                 size: 20
             },
             headers : {
                 Authorization: 'KakaoAK 690d2d6fc7114cf6b223af8e7ab60f4f'
             }
         }
-    )
+    ).done(function(books) {
+        const bookItems = books.documents
+        console.log(bookItems);
+
+        const bookItem = bookItems.map(item => {
+            return `<div class="book">
+                        <img src="${item.thumbnail}" alt="">
+                        <h4>${item.title}</h4>
+                        <p>가격 : ${item.price}원</p>
+                        <p>출판사 : ${item.publisher}</p>
+                    </div>`
+        }).join('')
+
+        bookContainer.innerHTML = bookItem
+    })
 }
 
 getBooks()
+
+btn.addEventListener('click', function(e) {
+    e.preventDefault()
+    if(input.value === '') return
+    bookName = input.value
+    getBooks()
+    input.value = ''
+})

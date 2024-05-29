7ff63f78b028709a864aba332dd507b082935f76
harimbu <harimbu@gmail.com>
2024-05-29 10:49:09 +0900

    init

---
+++ tutorial/12_gallery_app/app.js
@@ -0,0 +1,8 @@
+// https://api.pexels.com/v1/curated?per_page=1
+// https://api.pexels.com/v1/search?query=nature&per_page=1
+// 563492ad6f91700001000001c5e6480855e74c238b20eac4d3d528f9
+
+const API_KEY = '563492ad6f91700001000001c5e6480855e74c238b20eac4d3d528f9'
+const galleryDiv = document.querySelector('.gallery')
+const form = document.querySelector('form')
+const loadMore = document.querySelector('.load_more')
